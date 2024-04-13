4b0e49e3834931416489975a9379e1a74620c777
Sandeepan Mazumdar <rony99sandy@outlook.com>
2024-04-13 15:10:17 +0530

    getting the movie data into the Redux store using custom hooks

--- src/Utils/Constant.js
+++ src/Utils/Constant.js
@@ -3,3 +3,12 @@ export const Netflix_LOGO =
 
 export const USER_AVTAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzVkZGJiYWI5MjdmMjFiNjM0ODIxYmUxN2IyOGI2NiIsInN1YiI6IjY2MWE0NzgwOTgyZjc0MDE4NjQ2YzY5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Qb4dJpjMTiroueb6djQ92rRZfR1HZ-VmXe1dBMm5ye0",
+  },
+};
